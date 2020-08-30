6c8c36dce44abc90a3c5efccd9fb6c1419fde99b
zanllp <qc@zanllp.cn>
2020-08-30 15:39:16 +0800

    style: fix code semi

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,11 @@
-import { sharedState } from './store';
+import { sharedState } from './store'
 
 export const num2color = (c: number) => {
-  const r = c >> 16;
-  const g = (c >> 8) & 0xff;
-  const b = c & 0xff;
-  return [r, g, b];
-};
+  const r = c >> 16
+  const g = (c >> 8) & 0xff
+  const b = c & 0xff
+  return [r, g, b]
+}
 export type BlockState = {
   value?: string;
   rect: {
@@ -23,14 +23,14 @@ export type AnyBlockState = {
  * 共享状态里获取一个自增id
  */
 export const getIncrementId = (type: keyof typeof sharedState.idPool) => {
-  const arr = sharedState.idPool[type];
-  const id = arr.length !== 0 ? arr[arr.length - 1] + 1 : 0;
-  arr.push(id);
-  return id;
-};
+  const arr = sharedState.idPool[type]
+  const id = arr.length !== 0 ? arr[arr.length - 1] + 1 : 0
+  arr.push(id)
+  return id
+}
 
 export const createArray = <T>(num: number, createFn: (index: number) => T) => {
   return Array(num)
     .fill(null)
-    .map((_, i) => createFn(i));
-};
+    .map((_, i) => createFn(i))
+}
